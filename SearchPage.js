793301642d070f60f97342fea74f092b11ca35d9
hehaosen <307052084@qq.com>
2016-07-05 09:36:24 +0800

    +搜索的异步请求

--- SearchPage.js
+++ SearchPage.js
@@ -33,6 +33,7 @@ export default class SearchPage extends Component {
         this.state = {
             searchString: 'london',
             isLoading: false,
+            message: '',
         };
     }
     onSearchTextChanged(event) {
@@ -42,6 +43,23 @@ export default class SearchPage extends Component {
     _executeQuery(query) {
         console.log(query);
         this.setState({ isLoading: true });
+        fetch(query)
+            .then(response => response.json())
+            .then(json => this._handleResponse(json.response))
+            .catch(error =>
+                this.setState({
+                    isLoading: false,
+                    message: 'Something bad happened ' + error
+                }));
+    }
+
+    _handleResponse(response) {
+        this.setState({ isLoading: false , message: '' });
+        if (response.application_response_code.substr(0, 1) === '1') {
+            console.log('Properties found: ' + response.listings.length);
+        } else {
+            this.setState({ message: 'Location not recognized; please try again.'});
+        }
     }
 
     onSearchPressed() {
@@ -85,6 +103,8 @@ export default class SearchPage extends Component {
                 <Image source = {require('./src/house.png')}
                                style={styles.image}/>
                 {spinner}
+                <Text style={styles.description}>{this.state.message}</Text>
+
             </View>
         );
     }
